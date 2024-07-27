c3ebb6d4e3fa78585a970aaf08f0216c995fd716
Akshay R <akshayrajan345@gmail.com>
2024-07-27 17:39:13 +0530

    Add: Achievements list

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 import Badge from "./Badge";
 
-import BadgeLogo from "../assets/badge.png";
+import badge1 from "../assets/badge.png";
+import badge2 from "../assets/badge2.png";
+import badge3 from "../assets/badge3.png";
+
 
 export default function Achievements(props) {
   const container = {
@@ -22,7 +25,7 @@ export default function Achievements(props) {
     fontWeight: 'bold',
   };
 
-  const logo = [BadgeLogo];
+  const logo = [badge1, badge2, badge3];
     
   return (
     <>
@@ -31,7 +34,7 @@ export default function Achievements(props) {
         
         {props.content.map((achievement, index) => {
           console.log(achievement);
-          return <Badge key={index} logo={logo[0]} content={achievement} />
+          return <Badge key={index} logo={logo[index]} content={achievement} />
         })}
       </div>
     </>
--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,14 +1,42 @@
 import React from "react";
 
 export default function Badge(props) {
+
+    const container = {
+        display: 'flex',
+        font: `monospace`,
+        color: 'black',
+        fontSize: '15px',
+        margin: '15px 35px 0 0',
+        opacity: '0.8',
+        textAlign: 'left',
+        padding: '1rem',
+        border: '0.1px solid rgb(100, 100, 100)',
+        height: 'auto',
+        borderRadius: '10px',
+        backgroundColor: 'wheat',
+    };
+    const logo = {
+        width: '100px',
+        height: '100px',
+        margin: '0 10px 0 0',
+    };
+    const title = {
+        display: 'flex',
+        font: `monospace`,
+        fontSize: '20px',
+        textAlign: 'left',
+        fontWeight: 'bold',
+    };
+
     return (
-        <>
-            <img src={props.logo} alt="badge" />
+        <div style={container}>
+            <img src={props.logo} alt="badge" style={logo} />
             <div>
-                <div>{props.content.achievement}</div>
+                <div style={title}>{props.content.achievement}</div>
                 <div>{props.content.year}</div>
                 <div>{props.content.additional}</div>
             </div>
-        </>
+        </div>
     );
 }
