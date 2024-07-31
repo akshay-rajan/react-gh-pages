a5e6abca6c5ade224a59367e30b770cc52394b11
Akshay R <akshayrajan345@gmail.com>
2024-07-31 22:40:24 +0530

    Optimized code

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function Certificate({ certification, index }) {
   return (
-    <div class="card">
-      <div class="card__side card__side--front-1">
+    <div className="card">
+      <div className="card__side card__side--front-1">
         <div className="certification-card" key={index}><div>
           <div style={{color: "black", fontSize: "16px"}} className="card-content">
             {certification.certification}
@@ -18,7 +18,7 @@ export default function Certificate({ certification, index }) {
           </div>
         </div>
       </div>
-      <div className="card-content" class="card__side card__side--back card__side--back-1">
+      <div className="card-content card__side card__side--back card__side--back-1">
         <img src={certification.image} alt="certificate" style={{width: "100%", height: "100%"}} />
       </div>
     </div>
