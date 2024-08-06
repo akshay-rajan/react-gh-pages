f49584f57024ce572faa0d111feaad4b3c922717
Akshay R <akshayrajan345@gmail.com>
2024-08-06 22:01:11 +0530

    Test deploy: fix loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,14 +2,7 @@ import React from 'react';
 import loaderImage from '../assets/loader.gif';
 
 const Loader = () => (
-  <div id="preloader" style={{
-    width: '100%', 
-    height: '100%', 
-    display: 'flex', 
-    justifyContent: 'center', 
-    alignItems: 'center',
-    zIndex: '100',
-  }}>
+  <div id="preloader" className="loader">
     <img src={loaderImage} alt="Loading..." />
   </div>
 );
