2b83731d826db8f4c13b72bec1a5fa4df4c19771
Akshay Rajan <akshayrajan345@gmail.com>
2024-08-09 21:53:07 +0530

    Tested img on Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import BackgroundAnimation from './BackgroundAnimation';
 
-const Main = () => (
-  <div className="main" id="home">
-    <div className="bg"></div>
-    <div className="content first quarter">
-      <div className="name-container">
-        <div className="name-intro"></div>
-        <h1 className="name marquee">
-          <span className="m1">SOFTWARE DEVELOPER </span>
-          <span className="m2">SOFTWARE DEVELOPER </span>
-        </h1>
-        <div className="skills"></div>
+import "../styles/Main.css";
+
+import dp from "../assets/main.png";
+
+const Main = () => {
+
+  return (
+    <div className="main" id="home">
+      <div className="bg"></div>
+      <div className="content first quarter">
+        <div className="name-container">
+          <div className="name-intro imgDiv">
+            <img src={dp} alt="dp" className="main-img" />
+          </div>
+          <h1 className="name marquee">
+            <span className="m1">SOFTWARE DEVELOPER </span>
+            <span className="m2">SOFTWARE DEVELOPER </span>
+          </h1>
+        </div>
       </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default Main;
